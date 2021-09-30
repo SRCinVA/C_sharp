f182abeb4c4dd5860272ae626f5edd3bd041360f
SRCinVA <scitarella@hotmail.com>
2021-09-29 21:29:53 -0400

    Basic set up plus a few lines

---
+++ Dice_game.cs
@@ -0,0 +1,29 @@
+using System
+
+namespace Dice_game{
+
+
+    class Program{
+
+        static void Main(string[] args)
+        {
+            int playerRandomNum;
+            Random random = new Random() /* the constructor for new random numbers */
+
+            for (int i = 0; i < 10, i++)
+            {
+                Console.WriteLine("Press any key to roll the dice.")
+                playerRandomNum = random.Next(1,7); /* inclusive lower bound but exclusive upper bound*/
+            }
+
+
+
+
+        }
+
+
+
+    }
+
+
+}
