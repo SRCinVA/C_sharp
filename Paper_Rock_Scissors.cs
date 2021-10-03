5cccf3f725d9c40df0675d3318f53c0eb8397bfd
SRCinVA <scitarella@hotmail.com>
2021-10-03 18:33:53 -0400

    Started a few lines

---
+++ Paper_Rock_Scissors.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SystemText;
+using SystemThreading.Tasks;
+using System.Math;
+
+public class Program
+{
+	public static void Main()
+	{
+
+        String inputPlayer, inputCPU;
+        int randomInt;
+
+        COnsole.WriteLine("Choose between rock, paper, or scissor.")
+        inputPlayer - Console.ReadLine();
+
+	}
+}
