0fda2b8972398fa924fd32ed6ab5e9d67cf2eaf5
SRCinVA <scitarella@hotmail.com>
2021-10-02 20:14:11 -0400

    Added block of if statements

--- A_slightly_more_advanced_calculator.cs
+++ A_slightly_more_advanced_calculator.cs
@@ -12,15 +12,48 @@ namespace Calculator
             
             int num1;
             int num2;
+            float result;
+
+            string answer;
 
             Console.WriteLine("Hello! Welcome to our calculator!");
             Console.WriteLine("Please enter your first number: ");
             num1 = Convert.IntTo32(Console.ReadLine());
+        
 
             Console.WriteLine("Now enter your second number: ");
             num2 = Convert.IntTo32(Console.ReadLine());
 
+            Console.WriteLine("What type of operation would you like to do?");
+            Console.WriteLine("Press 'a' for addition, 's' for subtraction, 'm' for multiplication, and 'd' for division.");
+
+            answer = Console.ReadLine();  // no need to cast this from being a string.
+
+            if (answer == "a")
+            {
+                result = num1 + num2;
+            }
+
+            else if (answer == "s")
+            {
+                result = num1 - num2;
+            }
+
+            else if (answer == "m")
+            {
+                result = num1 * num2;
+            }
+
+            else (answer == "d")
+            {
+                result = num1/num2;
+            }
+
+
             Console.ReadKey();
+
+
+
         }
 
     }
