bddb1a66f00b6f9987169b7f544dbf17dbbb5990
SRCinVA <scitarella@hotmail.com>
2021-10-03 08:33:42 -0400

    Finished it up

--- A_slightly_more_advanced_calculator.cs
+++ A_slightly_more_advanced_calculator.cs
@@ -1,31 +1,26 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Calculator
 {
     class Program {
 
-        static void Main(string[] args){
+        static void Main(){
             
             int num1;
             int num2;
-            float result;
+            int result;
 
             string answer;
 
             Console.WriteLine("Hello! Welcome to our calculator!");
             Console.WriteLine("Please enter your first number: ");
-            num1 = Convert.IntTo32(Console.ReadLine());
+            num1 = Convert.ToInt32(Console.ReadLine());
         
-
             Console.WriteLine("Now enter your second number: ");
-            num2 = Convert.IntTo32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("What type of operation would you like to do?");
-            Console.WriteLine("Press 'a' for addition, 's' for subtraction, 'm' for multiplication, and 'd' for division.");
+            Console.WriteLine("Press 'a' for addition, 's' for subtraction, 'm' for multiplication, and any other key for division.");
 
             answer = Console.ReadLine();  // no need to cast this from being a string.
 
@@ -44,15 +39,15 @@ namespace Calculator
                 result = num1 * num2;
             }
 
-            else (answer == "d")
+            else // if we used an else it statement, then nothing would actually be calculated (it seems?)
             {
-                result = num1/num2;
+                result = num1 / num2;
             }
 
+            Console.WriteLine("The result is: " + result);
+            Console.WriteLine("Thank you for using this calculator program.");
 
             Console.ReadKey();
-
-
 
         }
 
