070d7166075fd575ff7036d20c48809baafd2a7b
SRCinVA <scitarella@hotmail.com>
2021-10-05 20:03:04 -0400

    massive debugging

--- Paper_Rock_Scissors.cs
+++ Paper_Rock_Scissors.cs
@@ -1,109 +1,99 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using SystemText;
-using SystemThreading.Tasks;
-using System.Math;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Program
+namespace RockPaperScissors
 {
-	public static void Main()
-	{
-
-        String inputPlayer, inputCPU;
-        int randomInt;
-
-        Console.WriteLine("Choose between rock, paper, or scissor.")
-        inputPlayer = Console.ReadLine();
-
-        Random rnd = new Random();
-
-        randomInt = rnd.Next(1,4);  // randomInt is equal to the next random number (perhaps recursive).
-                                    // bounded between 1 and 4.
-        switch (randomInt)
+    class Program
+    {
+        static void Main(string[] args)
         {
-            case 1:
-                inputCPU = "rock";
-                Console.WriteLine("The CPU chose 'rock'")
-                if (inputPlayer == 'rock')
-                {
-                    Console.WriteLine("It's a tie!\n\n");
-                }
 
-                else if (inputPlayer == 'paper')
-                {
-                    Console.WriteLine("The player wins this round!\n\n");
-                }
+            String inputPlayer, inputCPU;
+            int randomInt;
+            int scorePlayer = 0;
+            int scoreCPU = 0;
 
-                else (inputPlayer == 'scissors')
-                {
-                    Console.WriteLine("The CPU wins this round!\n\n");
-                }
+            Console.WriteLine("Choose between rock, paper, or scissor.");
+            inputPlayer = Console.ReadLine();
+            inputPlayer = inputPlayer.ToUpper();
+
+            Random rnd = new Random();
+
+            randomInt = rnd.Next(1,4);  // randomInt is equal to the next random number (perhaps recursive).
+                                        // bounded between 1 and 4.
+            switch (randomInt)
+            {
+                case 1:
+                    inputCPU = "rock";
+                    Console.WriteLine("The CPU chose 'rock'");
+                    if (inputPlayer == "rock")
+                    {
+                        Console.WriteLine("It's a tie!\n\n");
+                    }
+
+                    else if (inputPlayer == "paper")
+                    {
+                        Console.WriteLine("The player wins this round!\n\n");
+                        scorePlayer++;
+                    }
+
+                    else if(inputPlayer == "scissors")
+                    {
+                        Console.WriteLine("The CPU wins this round!\n\n");
+                        scoreCPU++;
+                    }
+                    break;
+
+                case 2:
+                    inputCPU = "paper";
+                    Console.WriteLine("The CPU chose 'paper'");
+                    if (inputPlayer == "rock")
+                    {
+                        Console.WriteLine("The CPU won this round!\n\n");
+                        scoreCPU++;
+                    }
+
+                    else if (inputPlayer == "paper")
+                    {
+                        Console.WriteLine("It's a tie!\n\n");
+                    }
+
+                    else if (inputPlayer == "scissors")
+                    {
+                        Console.WriteLine("The player won this round!");
+                        scorePlayer++;
+                    }
+                    break;
+
+                case 3:
+                    inputCPU = "scissors";
+                    Console.WriteLine("The CPU chose 'scissors'");
+                    if (inputPlayer == "rock")
+                    {
+                        Console.WriteLine("The player won this round!\n\n");
+                        scorePlayer++;
+                    }
+
+                    else if (inputPlayer == "paper")
+                    {
+                        Console.WriteLine("The CPU wins this round!\n\n");
+                        scoreCPU++;
+                    }
+
+                    else if (inputPlayer == "scissors")
+                    {
+                        Console.WriteLine("This round was a tie!\n\n");
+                    }
+                    break;
 
                 default:
-                {
-                    Console.WriteLine("I'm afraid that's an invalid entry ...")
-                }
-
-                break;
-
-            case 2:
-                inputCPU = "paper";
-                Console.WriteLine("The CPU chose 'paper'")
-                if (inputPlayer == 'rock')
-                {
-                    Console.WriteLine("The CPU won this round!\n\n");
-                }
-
-                else if (inputPlayer == 'paper')
-                {
-                    Console.WriteLine("It's a tie!\n\n");
-                }
-
-                else (inputPlayer == 'scissors')
-                {
-                    Console.WriteLine("The player won this round!");
-                }
-
-                default:
-                {
-                    Console.WriteLine("I'm afraid that's an invalid entry ...")
-                }
-
-                break;
-
-                break;
-
-            case 3:
-                inputCPU = "scissors";
-                Console.WriteLine("The CPU chose 'scissors'")
-                if (inputPlayer == 'rock')
-                {
-                    Console.WriteLine("The player won this round!\n\n");
-                }
-
-                else if (inputPlayer == 'paper')
-                {
-                    Console.WriteLine("The CPU wins this round!\n\n");
-                }
-
-                else (inputPlayer == 'scissors')
-                {
-                    Console.WriteLine("This round was a tie!\n\n");
-                }
-
-                default:
-                {
-                    Console.WriteLine("I'm afraid that's an invalid entry ...")
-                }
-
-                break;
-
-                break;
+                    Console.WriteLine("I'm afraid that's an invalid entry ...");
+                    break;
+            }                      
 
         }
-                                
-
-
-	}
+    }
 }
