aa7d6b88eb5d3d22cff558f2cf5c4ece3eacf759
SRCinVA <scitarella@hotmail.com>
2021-10-06 20:25:36 -0400

    Too many while loops

--- Paper_Rock_Scissors.cs
+++ Paper_Rock_Scissors.cs
@@ -15,85 +15,104 @@ namespace RockPaperScissors
             int randomInt;
             int scorePlayer = 0;
             int scoreCPU = 0;
+            bool playAgain = true;
 
-            Console.WriteLine("Choose between rock, paper, or scissor.");
-            inputPlayer = Console.ReadLine();
-            inputPlayer = inputPlayer.ToUpper();
-
-            Random rnd = new Random();
-
-            randomInt = rnd.Next(1,4);  // randomInt is equal to the next random number (perhaps recursive).
-                                        // bounded between 1 and 4.
-            switch (randomInt)
+            while (playAgain)
             {
-                case 1:
-                    inputCPU = "rock";
-                    Console.WriteLine("The CPU chose 'rock'");
-                    if (inputPlayer == "rock")
+                while (scorePlayer < 3 && scoreCPU < 3)
+                {
+                    Console.WriteLine("Choose between rock, paper, or scissor.");
+                    inputPlayer = Console.ReadLine();
+                    inputPlayer = inputPlayer.ToUpper();
+
+                    Random rnd = new Random();
+
+                    randomInt = rnd.Next(1,4);  // randomInt is equal to the next random number (perhaps recursive).
+                                                // bounded between 1 and 4.
+                    switch (randomInt)
                     {
-                        Console.WriteLine("It's a tie!\n\n");
-                    }
+                        case 1:
+                            inputCPU = "rock";
+                            Console.WriteLine("The CPU chose 'rock'");
+                            if (inputPlayer == "rock")
+                            {
+                                Console.WriteLine("It's a tie!\n\n");
+                            }
 
-                    else if (inputPlayer == "paper")
-                    {
-                        Console.WriteLine("The player wins this round!\n\n");
-                        scorePlayer++;
-                    }
+                            else if (inputPlayer == "paper")
+                            {
+                                Console.WriteLine("The player wins this round!\n\n");
+                                scorePlayer++;
+                            }
 
-                    else if(inputPlayer == "scissors")
-                    {
-                        Console.WriteLine("The CPU wins this round!\n\n");
-                        scoreCPU++;
-                    }
-                    break;
+                            else if(inputPlayer == "scissors")
+                            {
+                                Console.WriteLine("The CPU wins this round!\n\n");
+                                scoreCPU++;
+                            }
+                            break;
 
-                case 2:
-                    inputCPU = "paper";
-                    Console.WriteLine("The CPU chose 'paper'");
-                    if (inputPlayer == "rock")
-                    {
-                        Console.WriteLine("The CPU won this round!\n\n");
-                        scoreCPU++;
-                    }
+                        case 2:
+                            inputCPU = "paper";
+                            Console.WriteLine("The CPU chose 'paper'");
+                            if (inputPlayer == "rock")
+                            {
+                                Console.WriteLine("The CPU won this round!\n\n");
+                                scoreCPU++;
+                            }
 
-                    else if (inputPlayer == "paper")
-                    {
-                        Console.WriteLine("It's a tie!\n\n");
-                    }
+                            else if (inputPlayer == "paper")
+                            {
+                                Console.WriteLine("It's a tie!\n\n");
+                            }
 
-                    else if (inputPlayer == "scissors")
-                    {
-                        Console.WriteLine("The player won this round!");
-                        scorePlayer++;
-                    }
-                    break;
+                            else if (inputPlayer == "scissors")
+                            {
+                                Console.WriteLine("The player won this round!");
+                                scorePlayer++;
+                            }
+                            break;
 
-                case 3:
-                    inputCPU = "scissors";
-                    Console.WriteLine("The CPU chose 'scissors'");
-                    if (inputPlayer == "rock")
-                    {
-                        Console.WriteLine("The player won this round!\n\n");
-                        scorePlayer++;
-                    }
+                        case 3:
+                            inputCPU = "scissors";
+                            Console.WriteLine("The CPU chose 'scissors'");
+                            if (inputPlayer == "rock")
+                            {
+                                Console.WriteLine("The player won this round!\n\n");
+                                scorePlayer++;
+                            }
 
-                    else if (inputPlayer == "paper")
-                    {
-                        Console.WriteLine("The CPU wins this round!\n\n");
-                        scoreCPU++;
-                    }
+                            else if (inputPlayer == "paper")
+                            {
+                                Console.WriteLine("The CPU wins this round!\n\n");
+                                scoreCPU++;
+                            }
 
-                    else if (inputPlayer == "scissors")
-                    {
-                        Console.WriteLine("This round was a tie!\n\n");
-                    }
-                    break;
+                            else if (inputPlayer == "scissors")
+                            {
+                                Console.WriteLine("This round was a tie!\n\n");
+                            }
+                            break;
 
-                default:
-                    Console.WriteLine("I'm afraid that's an invalid entry ...");
-                    break;
-            }                      
-
+                        default:
+                            Console.WriteLine("I'm afraid that's an invalid entry ...");
+                            break;
+                    }                      
+                }
+                if (scorePlayer == 3)
+                {
+                    Console.WriteLine("The player has won!!");
+                }
+                else if (scoreCPU == 3)
+                {
+                    Console.WriteLine("The CPU has won!!");
+                }
+                else
+                {
+                    Console.WriteLine("It's a tie!");
+                }
+                Console.WriteLine("Would you like to play again? (y/n)")
+            }
         }
     }
 }
