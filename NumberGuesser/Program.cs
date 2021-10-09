ea6b3f18a7fef8be9d902be3986451e35c539acb
SRCinVA <scitarella@hotmail.com>
2021-10-08 21:09:34 -0400

    just basics started

---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+    class MainClass
+    {
+        //Entry point for our application
+        public static void Main(string[] args)
+        {
+            string appName = "Number Guesser";
+            string Version = "1.0.0";
+            string appAuthor = "Brad Traversy";
+
+        }
+    }
+}
