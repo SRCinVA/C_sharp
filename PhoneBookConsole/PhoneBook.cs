2353901f7d2f75b6efbab835790042b91a79d4cb
SRCinVA <scitarella@hotmail.com>
2021-10-16 16:23:48 -0400

    Got some basics started

---
+++ PhoneBookConsole/PhoneBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace PhoneBookConsole
+{
+    public class PhoneBook
+    {
+        public PhoneBook() // will manage the contact list
+        {
+            private List<Contact> _contacts { get; set; } // underscore indicates that it's private
+
+
+            public void AddContact(Contact contact)
+            {
+            _contacts.Add(contact);  // all we need to do is add the contact to the list _contacts
+            }
+    }
+}
---
+++ PhoneBookConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PhoneBookConsole
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from the Console Phone Book app.");
+
+            
+
+
+        }
+    }
+}
