69e9c99b83f32b9a57b68b2fe6f44d0db6d0e69e
SRCinVA <scitarella@hotmail.com>
2021-10-17 19:53:37 -0400

    More methods

--- PhoneBookConsole/PhoneBook.cs
+++ PhoneBookConsole/PhoneBook.cs
@@ -1,16 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 namespace PhoneBookConsole
 {
     public class PhoneBook
     {
-        public PhoneBook() // will manage the contact list
+        // will manage the contact list
+        private List<Contact> _contacts { get; set; } // underscore indicates that it's private
+        private void DisplayContactDetails(Contact contact)
         {
-            private List<Contact> _contacts { get; set; } // underscore indicates that it's private
+            Console.WriteLine($"Contact {contact.Name},{contact.Number}");
+        }
 
 
-            public void AddContact(Contact contact)
-            {
+        public void AddContact(Contact contact)
+        {
             _contacts.Add(contact);  // all we need to do is add the contact to the list _contacts
+        }
+
+
+        public void DisplayContact(string number)
+        {
+            var contact = _contacts.FirstOrDefault(c => c.Number == number);  // note the labda expression to make sure the numbes match
+
+            // the contact could be null or some specific contact
+            if (contact == null)
+            {
+                Console.WriteLine("Contact not found");
             }
+            else  // otherwise the contact exists, so we'll display it
+            {
+                Console.WriteLine($"Contact {contact.Name},{contact.Number}");
+            }
+        }
+
+        public void DisplayAllContacts() // no need for a parameter
+        {
+            // iterate over every item in the Contacts list
+            foreach (var contact in _contacts)
+            {
+                Console.WriteLine($"Contact {contact.Name},{contact.Number}");
+            }
+        }
+
+
     }
 }
