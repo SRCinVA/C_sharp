410c3b66b996328809371d849e7ca4f8b3cb87f1
SRCinVA <scitarella@hotmail.com>
2021-10-21 19:48:23 -0400

    Very minor work on the switch statement

--- PhoneBookConsole/Program.cs
+++ PhoneBookConsole/Program.cs
@@ -8,7 +8,43 @@ namespace PhoneBookConsole
         {
             Console.WriteLine("Hello from the Console Phone Book app.");
 
-            
+            Console.WriteLine("Select operation");
+            Console.WriteLine("1.) Add Contact");
+            Console.WriteLine("2.) Display contact by number");
+            Console.WriteLine("3.) View all contacts");
+            Console.WriteLine("Search for contacts by a given name");
+
+            var userInput = Console.ReadLine(); // ... whatever that input is.
+
+            // next, let's do some switch statements
+
+            var phoneBook = new PhoneBook();
+
+            switch (userInput)
+            {
+                case "1": // to add a name
+                    Console.WriteLine("Contact name:");
+                    var name = Console.ReadLine();
+                    Console.WriteLine("Contact number:");
+                    var number = Console.ReadLine();
+                    break;
+
+                case "2":
+                    break;
+
+                case "3":
+                    break;
+
+                case "4":
+                    break;
+
+                default:
+                    Console.WriteLine("Select a valid operation, please.");
+                    break;
+
+
+
+            }
 
 
         }
